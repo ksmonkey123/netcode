0f1920dd9e5fe7a73ddd72bafca919d8bf529d74
Andreas Wälchli <andreas.waelchli@me.com>
2018-08-14 16:19:04 +0200

    Update Channel.java

--- src/main/java/ch/awae/netcode/Channel.java
+++ src/main/java/ch/awae/netcode/Channel.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.concurrent.locks.Lock;
 
 /**
