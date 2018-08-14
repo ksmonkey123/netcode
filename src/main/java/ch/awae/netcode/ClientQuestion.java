46c540a806a5855aa83019f2d4cf14970600a239
Andreas Wälchli <andreas.waelchli@me.com>
2018-08-14 17:34:22 +0200

    Update ClientQuestion.java

--- src/main/java/ch/awae/netcode/ClientQuestion.java
+++ src/main/java/ch/awae/netcode/ClientQuestion.java
@@ -9,14 +9,18 @@ import lombok.Getter;
 @AllArgsConstructor
 final class ClientQuestion implements Serializable {
 	private final static long serialVersionUID = 1L;
-	private final long commandId;
+	private final long questionId;
 	private final Serializable data;
+	
+	ClientAnswer response(Serializable data) {
+	    return new ClientAnswer(questionId, data);
+	}
 }
 
 @Getter
 @AllArgsConstructor
 final class ClientAnswer implements Serializable {
 	private final static long serialVersionUID = 1L;
-	private final long commandId;
+	private final long questionId;
 	private final Serializable data;
 }
