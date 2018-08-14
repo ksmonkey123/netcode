6c2f1ff13214a51f9cc8b8402edeb0e129b1da68
Andreas Wälchli <andreas.waelchli@me.com>
2018-08-14 16:16:59 +0200

    made Channel fully concurrent

--- src/main/java/ch/awae/netcode/Channel.java
+++ src/main/java/ch/awae/netcode/Channel.java
@@ -1,9 +1,11 @@
 package ch.awae.netcode;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.Lock;
 
 /**
  * A single communications channel.
@@ -18,80 +20,92 @@ final class Channel {
 	private final String appId;
 	private final String creator;
 
-	private HashMap<String, ClientHandler> clients = new HashMap<>();
-	private AtomicInteger member = new AtomicInteger(0);
-	private AtomicBoolean open = new AtomicBoolean(true);
+	private final ConcurrentHashMap<String, ClientHandler> clients = new ConcurrentHashMap<>();
+	private final AtomicInteger memberCount = new AtomicInteger(0);
+	private final AtomicBoolean open = new AtomicBoolean(true);
+	
+	private final Lock JOIN_LOCK;
+	private final Lock CLOSE_LOCK;
 
 	Channel(String appId, ChannelConfiguration config, ChannelManager owner, String userId) {
 		this.config = config;
 		this.owner = owner;
 		this.appId = appId;
 		this.creator = userId;
+		// create R/W lock
+		ReadWriteLock lock = new ReentrantReadWriteLock();
+        this.JOIN_LOCK = lock.readLock();
+        this.CLOSE_LOCK = lock.writeLock();
 	}
 
 	boolean isFull() {
-		return member.get() >= config.getMaxClients();
+		return memberCount.get() >= config.getMaxClients();
 	}
 
-	synchronized void join(String userId, ClientHandler handler) throws IOException, ConnectionException {
+	void join(String userId, ClientHandler handler) throws IOException, ConnectionException, InterruptedException {
 		if (!open.get())
 			throw new IllegalStateException();
-		if (isFull())
-			throw new ChannelUserLimitReachedException("channel limit reached: " + config.getMaxClients());
-		ClientHandler old = clients.putIfAbsent(userId, handler);
-		if (old != null)
-			throw new DuplicateUserIdException("duplicate username: '" + userId + "'");
-		member.incrementAndGet();
-		sendGreetingMessage(handler);
-		notifyUserJoined(userId);
+		JOIN_LOCK.lock();
+		try {
+		    if (!open.get())
+    			throw new IllegalStateException();
+		    if (memberCount.getAndIncrement() >= config.getMaxClients()) {
+			    memberCount.getAndDecrement();
+			    throw new ChannelUserLimitReachedException("channel limit reached: " + config.getMaxClients());
+		    }
+		    // try to insert
+		    if (clients.putIfAbsent(userId, handler) != null) {
+		        memberCount.getAndDecrement();
+			    throw new DuplicateUserIdException("duplicate username: '" + userId + "'");
+		    }
+		    // send base data and alert members
+		    sendGreetingMessage(handler);
+		    notifyUserJoined(userId);
+		} finally {
+		    JOIN_LOCK.unlock();
+		}
 	}
 	
-	private void sendGreetingMessage(ClientHandler handler) throws IOException {
-	    String[] users = clients.keySet().toArray(new String[0]);
-		handler.send(MessageFactory.serverMessage(new GreetingMessage(config, users)));
-	}
-	
-	private void notifyUserJoined(String userId) {
-	    Message msg = MessageFactory.serverMessage(new UserChange(userId, true));
-		clients.values().forEach(c -> {
-			if (!c.getUserId().equals(userId))
-				try {
-					c.send(msg);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		});
-	}
-
-	synchronized void quit(String userId) throws IOException {
+	void quit(String userId) throws IOException {
 		ClientHandler client = clients.remove(userId);
 		if (client == null)
 			return;
 		client.close();
 		send(MessageFactory.serverMessage(new UserChange(userId, false)));
-		if (member.decrementAndGet() <= 0)
+		if (memberCount.decrementAndGet() <= 0)
 			owner.closeChannel(appId, config.getChannelId());
 	}
 
-	synchronized void close() throws IOException {
+	void close() throws IOException, InterruptedException {
 		if (!open.compareAndSet(true, false))
 			return;
-		for (String user : clients.keySet().toArray(new String[0]))
-			quit(user);
+		CLOSE_LOCK.lock();
+		try {
+		    for (String user : clients.keySet().toArray(new String[0]))
+			    quit(user);
+		} finally {
+		    CLOSE_LOCK.unlock();
+		}
 	}
 
-	ChannelInformation getInfo() {
-		return new ChannelInformation(config.getChannelId(), config.getChannelName(), creator, member.get(),
-				config.getMaxClients(), config);
-	}
-
-	synchronized void send(MessageImpl msg) throws IOException {
+	void send(MessageImpl msg) {
 		if (msg.isPrivateMessage())
 		    sendPrivateMessage(msg);
 		else
 			sendPublicMessage(msg);
 	}
+
+	String[] getMembers() {
+		return clients.keySet().toArray(new String[0]);
+	}
 	
+	ChannelInformation getInfo() {
+		return new ChannelInformation(config.getChannelId(), config.getChannelName(),
+		            creator, memberCount.get(), config.getMaxClients(), config);
+	}
+
+    /* ### MESSAGE SENDING SUBROUTINES ### */
+    
 	private void sendPrivateMessage(MessageImpl msg) {
 	    ClientHandler client = clients.get(msg.getTargetId());
 	    if (client != null)
@@ -112,9 +126,22 @@ final class Channel {
 				}
 		});
 	}
-
-	String[] getMembers() {
-		return clients.keySet().toArray(new String[0]);
+	
+	private void sendGreetingMessage(ClientHandler handler) throws IOException {
+	    String[] users = clients.keySet().toArray(new String[0]);
+		handler.send(MessageFactory.serverMessage(new GreetingMessage(config, users)));
+	}
+	
+	private void notifyUserJoined(String userId) {
+	    Message msg = MessageFactory.serverMessage(new UserChange(userId, true));
+		clients.values().forEach(c -> {
+			if (!c.getUserId().equals(userId))
+				try {
+					c.send(msg);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		});
 	}
 
 }
