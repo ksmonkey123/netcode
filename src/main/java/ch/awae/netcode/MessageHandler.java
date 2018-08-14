b255b1e2786771038377d73d8b6b31b0e61d519f
Andreas Wälchli <andreas.waelchli@me.com>
2018-08-14 17:03:01 +0200

    Added more handler methods to the MessageHandler

--- src/main/java/ch/awae/netcode/MessageHandler.java
+++ src/main/java/ch/awae/netcode/MessageHandler.java
@@ -10,5 +10,19 @@ package ch.awae.netcode;
 public interface MessageHandler {
 
 	void handleMessage(Message msg);
+	
+	/**
+	 * handles private messages
+	 * 
+	 * Since 2.0.0 it is possible to define separate handlers for private messages.
+	 * By default this simply forwards to {@link #handleMessage(Message)}.
+	 */
+	default void handlePrivateMessage(Message msg, String from) {
+	    handleMessage(msg);
+	}
+	
+	default Serializable handleQuestion(Serializable data) {
+	    throw new UnsupportedOperationException("questions not supported by this client");
+	}
 
 }
