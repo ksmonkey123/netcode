3ea7c93c842fdd2a16556e580abdafb6c9a7b5c2
Andreas Wälchli <andreas.waelchli@me.com>
2020-03-21 18:01:05 +0100

    introduced a separate thread for message handling and a thread pool for question handling

--- src/main/java/ch/awae/netcode/NetcodeServerFactory.java
+++ src/main/java/ch/awae/netcode/NetcodeServerFactory.java
@@ -24,8 +24,6 @@ import lombok.val;
  * The following configuration is assumed by default:
  * 
  * <ul>
- * <li>socketMode: TLS</li>
- * <li>securityMode: ANONYMOUS</li>
  * <li>appId validation: appIds of the pattern [a-zA-Z0-9_]+ are accepted</li>
  * <li>channelId generation: random String of the pattern [a-zA-Z0-9]{6}</li>
  * <li>enable public channels: true</li>
@@ -40,13 +38,9 @@ import lombok.val;
 @Getter
 public final class NetcodeServerFactory {
 
-	private SocketMode socketMode = SocketMode.TLS;
-	private SecurityMode securityMode = SecurityMode.ANONYMOUS;
 	private int maxClients = 50;
 	private Predicate<String> appIdValidator = s -> true;
 	private Supplier<String> channelIdProvider = new RandomStringGenerator(6);
-	@Getter(AccessLevel.NONE)
-	private List<Consumer<ServerSocket>> afterBind = new ArrayList<>();
 	private final int port;
 	private @Setter boolean enablePublicChannels = true;
 	private @Setter boolean enableServerCommands = true;
@@ -61,75 +55,15 @@ public final class NetcodeServerFactory {
 		this.port = port;
 	}
 
-	/**
-	 * Specifies both the socket mode and the security mode to use for new
-	 * connections. The socket mode specifies if a plain (unencrypted)
-	 * connection, SSL, TLS or both (SSL or TLS) should be used. The security
-	 * mode specifies for secured (TLS/SSL) sockets if an anonymous cipher, a
-	 * certificate-based cipher (or both) is acceptable. This information is
-	 * used to negociate a cipher that is acceptable to both the server and the
-	 * client.
-	 * 
-	 * If socketMode is set to {@link SocketMode#PLAIN}, then the securityMode
-	 * must be set to {@link SecurityMode#ANY}.
-	 * 
-	 * @throws IllegalArgumentException
-	 *             an illegal combination has been provided
-	 * @throws NullPointerException
-	 *             any parameter is null
-	 */
-	public void setMode(SocketMode socketMode, SecurityMode securityMode) {
-		Objects.requireNonNull(socketMode, "socketMode may not be null");
-		Objects.requireNonNull(securityMode, "securityMode may not be null");
-		this.socketMode = socketMode;
-		if (socketMode == SocketMode.PLAIN && securityMode != SecurityMode.ANY)
-			throw new IllegalArgumentException("incompatible securityMode");
-		this.securityMode = securityMode;
-	}
-
-	/**
-	 * Adds a function to the post-bind queue.
-	 * 
-	 * The post-bind queue allows access to the {@link ServerSocket} or
-	 * {@link SSLServerSocket} as soon as it is created (but for SSLSockets
-	 * before the handshake). This allows arbitrary modification of the socket
-	 * configuration. This is especially useful for SSLServerSockets where more
-	 * control over the security configuration may be desired.
-	 * 
-	 * If the socket mode is set to {@link SocketMode#PLAIN}, the passed socket
-	 * will be of type {@link ServerSocket}, for all other modes it will be a
-	 * {@link SSLServerSocket}.
-	 * 
-	 * @param runner
-	 *            the runner to add to the post-bind queue. may not be null.
-	 */
-	public void runAfterBind(Consumer<ServerSocket> runner) {
-		Objects.requireNonNull(runner);
-		afterBind.add(runner);
-	}
-
-	/**
-	 * specifies the connection backlog.
-	 * 
-	 * @see ServerSocket#ServerSocket(int, int)
-	 */
 	public void setMaxClients(int max) {
 		if (max <= 0)
 			throw new IllegalArgumentException("backlog must be positive");
 		this.maxClients = max;
 	}
 
-	/**
-	 * Start the server
-	 */
 	public NetcodeServer start() throws IOException {
-		ServerSocketFactory ssf = (socketMode == SocketMode.PLAIN) ? ServerSocketFactory.getDefault()
-				: SSLServerSocketFactory.getDefault();
+		ServerSocketFactory ssf = ServerSocketFactory.getDefault();
 		ServerSocket ss = ssf.createServerSocket(port, maxClients);
-		if (socketMode != SocketMode.PLAIN)
-			applySecuritySettings((SSLServerSocket) ss);
-		for (val f : afterBind)
-			f.accept(ss);
 		ServerCapabilities features = new ServerCapabilities(enablePublicChannels, enableServerCommands);
 		NetcodeServerImpl ns = new NetcodeServerImpl(ss, appIdValidator, channelIdProvider, features);
 		ns.start();
@@ -161,22 +95,6 @@ public final class NetcodeServerFactory {
 	public void setChannelIdProvider(Supplier<String> channelIdProvider) {
 		Objects.requireNonNull(channelIdProvider);
 		this.channelIdProvider = channelIdProvider;
-	}
-
-	private void applySecuritySettings(SSLServerSocket ss) {
-		List<String> ciphers = new ArrayList<>();
-		for (val c : ss.getSupportedCipherSuites()) {
-			if (socketMode == SocketMode.SSL && !c.startsWith("SSL"))
-				continue;
-			if (socketMode == SocketMode.TLS && !c.startsWith("TLS"))
-				continue;
-			if (securityMode == SecurityMode.CERTIFICATE && c.contains("_anon_"))
-				continue;
-			if (securityMode == SecurityMode.ANONYMOUS && !c.contains("_anon_"))
-				continue;
-			ciphers.add(c);
-		}
-		ss.setEnabledCipherSuites(ciphers.toArray(new String[0]));
 	}
 
 }
